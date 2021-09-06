8216d088a3a59457416f6cba41a31baa26358349
DevExpressExampleBot <fakeEmail>
2021-09-06 19:49:56 +0300

    Source auto update [skip ci]

--- VB/Window1.xaml.vb
+++ VB/Window1.xaml.vb
@@ -1,5 +1,4 @@
-﻿Imports Microsoft.VisualBasic
-#Region "usings"
+﻿#Region "usings"
 Imports System
 Imports System.Collections.Generic
 Imports System.ComponentModel
@@ -10,13 +9,17 @@ Imports DevExpress.Xpf.Grid
 Namespace ClearFocusedRow
 	Partial Public Class Window1
 		Inherits Window
+
 		Public Sub New()
 			InitializeComponent()
 
 '			#Region "Initialize the grid."
 			Dim list As IList(Of TestData) = New BindingList(Of TestData)()
 			For i As Integer = 0 To 9
-				list.Add(New TestData() With {.Text = String.Format("Row{0}", i), .Number = i})
+				list.Add(New TestData() With {
+					.Text = String.Format("Row{0}", i),
+					.Number = i
+				})
 			Next i
 
 			grid.ItemsSource = list
@@ -33,24 +36,8 @@ Namespace ClearFocusedRow
 
 	#Region "Test class to populate the grid."
 	Public Class TestData
-		Private privateNumber As Integer
 		Public Property Number() As Integer
-			Get
-				Return privateNumber
-			End Get
-			Set(ByVal value As Integer)
-				privateNumber = value
-			End Set
-		End Property
-		Private privateText As String
 		Public Property Text() As String
-			Get
-				Return privateText
-			End Get
-			Set(ByVal value As String)
-				privateText = value
-			End Set
-		End Property
 	End Class
 	#End Region
 End Namespace
